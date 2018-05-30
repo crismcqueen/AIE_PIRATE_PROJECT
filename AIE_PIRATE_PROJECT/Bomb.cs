46ebabee1ebd1529b348934740fbf33231a7d075
Joel-Walkemeyer <37867089+Joel-Walkemeyer@users.noreply.github.com>
2018-05-30 19:12:24 +0800

    bomb class

---
+++ AIE_PIRATE_PROJECT/Bomb.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIE_PIRATE_PROJECT
+{
+    class Bomb
+    {
+    }
+}
