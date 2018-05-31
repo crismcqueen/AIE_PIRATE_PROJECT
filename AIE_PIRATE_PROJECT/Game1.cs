569c1ef04780f52e71bc48c4b0c3ab0c293d71b5
Joel-Walkemeyer <37867089+Joel-Walkemeyer@users.noreply.github.com>
2018-05-31 20:43:14 +0800

    added monogame extended

--- AIE_PIRATE_PROJECT/Game1.cs
+++ AIE_PIRATE_PROJECT/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Utilities;
 using System;
 
 namespace AIE_PIRATE_PROJECT
