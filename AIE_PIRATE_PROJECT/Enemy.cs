de0bb5216548f5491d33be3e9df14e2bb492f15b
SianSallway <37866416+SianSallway@users.noreply.github.com>
2018-06-04 10:01:38 +1000

    Enemy class

---
+++ AIE_PIRATE_PROJECT/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace AIE_PIRATE_PROJECT
+{
+    class Enemy
+    {
+        Sprite sprite;
+
+        Game1 game = null;
+
+       /* public Vector2 Position
+        {
+
+
+        } */
+
+        public Enemy(Game1 game)
+        {
+
+
+        }
+
+        public void Load(ContentManager content)
+        {
+
+
+        }
+
+        public void Update(float deltaTime)
+        {
+
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+
+        }
+
+    }
+}
