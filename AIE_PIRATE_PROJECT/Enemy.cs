1c544e333a841fc04d826a03757c36ea92aa2c1f
SianSallway <37866416+SianSallway@users.noreply.github.com>
2018-06-27 20:55:01 +1000

    enemy editing
    sprite rotation in progress

--- AIE_PIRATE_PROJECT/Enemy.cs
+++ AIE_PIRATE_PROJECT/Enemy.cs
@@ -16,20 +16,18 @@ namespace AIE_PIRATE_PROJECT
 {
     class Enemy
     {
-       // Sprite sprite = new Sprite(); 
         Game1 game = null;
         private Vector2 enemyPosition;
         
         protected int health;
         protected int speed= 5;
         protected int radius;
+        float enemyRotation = 0;
+        float enemyTurnSpeed = 1;
+        float xSpeed = 0;
+        float ySpeed = 0;
 
         public static List<Enemy> enemies = new List<Enemy>();
-
-        //public Transform2D player;
-        //int maxDistance = 10;
-        //int minDistance = 5;
-
 
         public Vector2 Position
         {
@@ -83,34 +81,31 @@ namespace AIE_PIRATE_PROJECT
             Vector2 enemyVelocity = direction * speed * Game1.maxVelocity * dt;
 
 
-            enemyPosition += enemyVelocity*dt;
 
-            
-
+            double x = (xSpeed * Math.Cos(enemyRotation)) - (ySpeed * Math.Sin(enemyRotation));
+            double y = (xSpeed * Math.Sin(enemyRotation)) + (ySpeed * Math.Cos(enemyRotation));
         }
 
         private void ChasePlayer()
         {
+            
             foreach (Enemy e in enemies)
             {
 
-                /*if (IsColliding() == true)
+                /*if (IsColliding(e.radius, playerPosition ) == true)
                 {
+                    //face player 
                     //fire at player
                     //projectiles yet to be implemented
 
-                } 
-                else 
+                }
+                else
                 {
-                    
-                    
+                    //don't fire at player
+
                 } */
-             
-                   
 
-               
             }
-
         } 
 
         protected bool IsColliding (Vector2 playerPosition, float radius1, Vector2 enemyPosition, float radius2)
@@ -121,22 +116,20 @@ namespace AIE_PIRATE_PROJECT
             if (radius1 + radius2 <= length)
             {
                 return false;
-
             }
 
             return true;
+
+
         }
-
-
+        
         public void Draw(SpriteBatch spriteBatch)
         {
-            
-
-
+         
         }
-
-
+        
     }
+
     class enemyShip : Enemy
     {
         public enemyShip(Vector2 newPos) : base(newPos)
@@ -152,61 +145,4 @@ namespace AIE_PIRATE_PROJECT
         }
     }
 }
-/*
-        Sprite sprite;
-        Game1 game = null;
-        Vector2 enemyOffset = new Vector2(0, 0);
-        Vector2 enemyPosition = new Vector2(200, 100);
 
-        public Transform Player;
-        int maxDistance = 10;
-        int minDistance = 5; 
-        
-
-        float enemySpeed = 100;
-        float enemyRadius = 60;
-        float enemyRotations = 4.5f;
-
-       public Vector2 Position
-        {
-            get { return sprite.position; }
-
-
-        } 
-
-
-        public Enemy(Game1 game)
-        {
-            this.game = game;
-
-        }
-
-        public void Load(ContentManager content)
-        {
-
-
-        }
-
-        public void Update(float deltaTime)
-        {
-            transform.LookAt(Player);
-
-            if (Vector3.Distance(transform.position, Player.position) >= MinDist)
-            {
-                transform.position += transform.forward * enemySpeed * Time.deltaTime;
-
-                if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
-                {
-                    //shoot at player
-                }
-
-            } 
-            
-        }
-
-        
-
-        public void Draw(SpriteBatch spriteBatch)
-        {
-            
-        }*/
