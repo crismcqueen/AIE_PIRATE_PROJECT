96f987d5621180ce5d6657aab1f395efcc4b4592
Joel-Walkemeyer <37867089+Joel-Walkemeyer@users.noreply.github.com>
2018-07-11 15:46:05 +0800

    remove orange debug dots

--- AIE_PIRATE_PROJECT/Game1.cs
+++ AIE_PIRATE_PROJECT/Game1.cs
@@ -204,8 +204,6 @@ namespace AIE_PIRATE_PROJECT
             // world space here
             spriteBatch.Begin(transformMatrix: cam.GetViewMatrix(), samplerState: SamplerState.PointClamp);
 
-            ShapeExtensions.DrawPoint(spriteBatch, Vector2.Zero, Color.Orange, 3);
-
             player.playerOffset = new Vector2(playerSprite.Width / 2, playerSprite.Height / 2);
             //spriteBatch.Draw(splashScreen, new Vector2(0, 0), Color.Silver);
 
@@ -217,12 +215,10 @@ namespace AIE_PIRATE_PROJECT
             }
             //spriteBatch.Draw(playerSprite, player.Position, Color.White);
             spriteBatch.Draw(playerSprite, player.PlayerPosition, null, Color.White, player.playerRotation, player.playerOffset, 1, SpriteEffects.None, 0);
-            ShapeExtensions.DrawPoint(spriteBatch, player.PlayerPosition, Color.Orange, 3);
 
             foreach (Enemy e in enemies)
             {
                 spriteBatch.Draw(e.texture, e.Position, null, Color.White, e.enemyRotation, new Vector2(e.texture.Width / 2, e.texture.Height / 2), 1, SpriteEffects.None, 1);
-                ShapeExtensions.DrawPoint(spriteBatch, e.Position, Color.Orange, 3);
             }
             
             
